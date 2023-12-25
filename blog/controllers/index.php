1ceea7cd7c4237b2a3b27a65b5024bd353ca0b69
mahabub171 <75315989+mahabub171@users.noreply.github.com>
2023-12-25 14:49:35 +0600

    Update index.php

--- blog/controllers/index.php
+++ blog/controllers/index.php
@@ -4,7 +4,7 @@ $db = App::resolve(Database::class);
 $heading = 'Blog';
 
 //$posts = $db->query('select * from posts where post_status = "Publish"')->fetchALL();
-$posts = $db->query("SELECT * FROM `posts` WHERE `user_id` = $id AND `publish_date` <CURRENT_DATE()")->fetchALL();
+$posts = $db->query("SELECT * FROM `posts` WHERE `user_id` = $id AND `publish_date<CURRENT_DATE()")->fetchALL();
 
 $comment = $db->query('select * from comment')->fetchAll();
 require "views/index.view.php";
